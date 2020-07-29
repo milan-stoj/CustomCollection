97fc9089d2a0237ab361c0e090ba792377226fa0
MILAN STOJANOVIC <milanstoj94@gmail.com>
2020-07-29 13:32:24 -0500

    added unit test for zip() method

---
+++ CustomListTest/ZipMethodTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace CustomListTest
+{
+    [TestClass]
+    public class ZipMethodTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
