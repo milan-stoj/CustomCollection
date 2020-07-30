25257e6e879aa7c709977a6de3bc92b95affbba2
MILAN STOJANOVIC <milanstoj94@gmail.com>
2020-07-30 17:13:58 -0500

    documentation complete for -operator

--- CustomList/Program.cs
+++ CustomList/Program.cs
@@ -10,7 +10,13 @@ namespace CustomList
     {
         static void Main(string[] args)
         {
-
+            LiteList<int> listA = new LiteList<int>() { 15, 20, 25 };
+            LiteList<int> listB = new LiteList<int>() { 12, 25, 14 };
+            LiteList<int> newList = listA - listB;
+            foreach(int item in newList)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
