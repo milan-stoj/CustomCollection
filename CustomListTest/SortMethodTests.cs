f1301e7b97328c016317144607ffd394778ba44d
MILAN STOJANOVIC <milanstoj94@gmail.com>
2020-07-30 13:07:14 -0500

    added test for Sort() method

---
+++ CustomListTest/SortMethodTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace CustomListTest
+{
+    [TestClass]
+    public class SortMethodTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
