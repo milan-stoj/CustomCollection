1e8134f0f2cfdec5ceaa151c4b51e7fb82ea9b6e
MILAN STOJANOVIC <milanstoj94@gmail.com>
2020-07-30 16:40:17 -0500

    polishing LiteList class

--- CustomList/LiteList.cs
+++ CustomList/LiteList.cs
@@ -10,12 +10,13 @@ namespace CustomList
 {
     public class LiteList<T> : IEnumerable
     {
-        // Member variables
+        // --- CLASS INSTANTIATION --- 
+        // member variables
         private int count;
         private int capacity;
         private T[] items;
 
-        // Member properties
+        // member properties
         public T this[int index]                    // Declaring public indexer
         {
             get => items[index];
@@ -24,20 +25,22 @@ namespace CustomList
         public int Count { get => count; }          // Declaring public Count property
         public int Capacity { get => capacity; }    // Declaring public Capacity property
 
-        // Constructor
+        // constructor
         public LiteList()
         {
             count = 0; capacity = 4;
             items = new T[capacity];
         }
 
-        // Public LiteList methods
+        // --- PUBLIC LITE LIST METHODS ---
+        // adds an item to the list
         public void Add(T item)
         {
             items[count] = item; count++;
             CheckListSize();
         }
 
+        // removes the last member of the list
         public void RemoveLast()
         {
             items[count] = default(T);
@@ -52,7 +55,8 @@ namespace CustomList
             }
         }
 
-        public void Remove(T target) // Search for specific thing and remove one instance of it.
+        // search for specific thing and remove one instance of it
+        public void Remove(T target) 
         {
             bool removed = false;
             T[] tempList = new T[capacity];
@@ -75,6 +79,7 @@ namespace CustomList
             count--;
         }
 
+        // zips the lists together (i.e. {1,2,3}.zip({5,6,7}) = {1,5,2,6,3,7})
         public LiteList<T> Zip(LiteList<T> zipList)
         {
             int maxCount = Math.Max(count, zipList.count);
@@ -87,6 +92,8 @@ namespace CustomList
             return newList;
         }
 
+        // overrides the ToString method
+        // converts each object to a string and encapsulates with brackets
         public override string ToString()
         {
             string tempString = "";
@@ -97,6 +104,7 @@ namespace CustomList
             return tempString;
         }
 
+        // overrides the "+" operator and allows lists to be added together
         public static LiteList<T> operator +(LiteList<T> a, LiteList<T> b)
         {
             int maxCount = Math.Max(a.count, b.count);
@@ -111,13 +119,14 @@ namespace CustomList
             return newList;
         }
 
+        // overrides the "-" operator and allows lists to be subtracted
         public static LiteList<T> operator -(LiteList<T> a, LiteList<T> b) 
         {
             int maxCount = Math.Max(a.count, b.count);
             LiteList<T> newList = new LiteList<T>();
             for (int i = 0; i < maxCount; i++)
             {
-                // Dynamic keyword allows operation to be resolved at run-time.
+                // dynamic keyword allows operation to be resolved at run-time.
                 dynamic x = a[i];      
                 dynamic y = b[i];
                 newList.Add(x - y);
@@ -125,26 +134,26 @@ namespace CustomList
             return newList;
         }
 
-        // Sorting using Bubble-sort algorithm:
+        // sorts list using bubble-sort algorithm
         public void Sort()      
         {
-            int i;
-            int j;
+            int i; int j;
             int count = this.count;
             for (j = count - 1; j > 0; j--)
             {
                 for (i = 0; i < j; i++)
                 {
-                    if (Compare(this[i], this[i+1]) > 0) // > 0 means 
+                    if (Compare(this[i], this[i+1]) > 0)
                     {
-                        SwapElement(this, i, i + 1);
+                        SwapElement(i, i + 1);
                     }
                 }
             }
         }
 
-        // Private LiteList methods
-        private void CheckListSize()    // Checks for count == capacity, and runs DoubleListSize method
+        // --- PRIVATE LITELIST METHODS ---
+        // checks for count == capacity, and runs DoubleListSize method
+        private void CheckListSize()   
         {
             if (count == capacity)
             {
@@ -152,22 +161,27 @@ namespace CustomList
             }
         }
 
+        // compares two objects of type T.
+        // returns 1 if a greater than b.
+        // returns 0 if equal.
+        // returns -1 if a less than b.
         private int Compare(T a, T b)
         {
             Comparer<T> comparer = Comparer<T>.Default;
             return comparer.Compare(a, b);
         }
 
-        private void SwapElement(LiteList<T> items, int indexA, int indexB)
+        // swaps element of list
+        private void SwapElement(int indexA, int indexB)
         {
             T tempObject;
-            tempObject = items[indexA];
-            items[indexA] = items[indexB];
-            items[indexB] = tempObject;
+            tempObject = this[indexA];
+            this[indexA] = this[indexB];
+            this[indexB] = tempObject;
         }
 
-
-        private void DoubleListSize()   // Doubles public array items capacity
+        // Doubles public array items capacity
+        private void DoubleListSize()
         {
             capacity *= 2;                        
             T[] tempList = new T[capacity];
@@ -176,11 +190,6 @@ namespace CustomList
                 tempList[i] = items[i];
             }
             items = tempList;
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
         }
     }
 }
